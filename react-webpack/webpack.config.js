b55ba42f384b20a2b98b6fd869430c5ba1287e5f
Victor Roberto <robertovictor0702@gmail.com>
2023-07-13 23:58:10 -0300

    react hot loader adicionado e utilizando server com node

--- react-webpack/webpack.config.js
+++ react-webpack/webpack.config.js
@@ -1,21 +1,32 @@
-'use strict'
+"use strict";
 
-const path = require('path')
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    devtool: 'source-map',
-    entry: path.join(__dirname, 'src', 'index'),
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: 'dist/'
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            include: /src/,
-            loader: 'babel'
-        }]
-    }
-}
+  devtool: "source-map",
+  entry: [
+    "react-hot-loader/patch",
+    "webpack-dev-server/client?http://localhost:3000",
+    "webpack/hot/only-dev-server",
+    path.join(__dirname, "src", "index"),
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "dist/",
+  },
+
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        loader: "babel",
+      },
+    ],
+  },
+};
