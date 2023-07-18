211390b34a06cfbde9bfa1c011c4ba3fa1d0a46b
Victor Roberto <robertovictor0702@gmail.com>
2023-07-18 20:36:31 -0300

    atributos HTML diferentes em JSX

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -6,7 +6,12 @@ import Title from './title'
 const App = React.createClass({
   render: function () {
     return (
+      <div className='container'>
         <Title name='Victor Roberto' />
+        <label htmlFor='input'>Input</label>
+        <input type='text' id='input'/>
+      </div>
+        
     )
   }
 })
