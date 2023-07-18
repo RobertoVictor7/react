9399e2f8be5cc3520843dc4c3aa7da9fe54b0c25
Victor Roberto <robertovictor0702@gmail.com>
2023-07-18 20:43:03 -0300

    getDefaultProps

--- react-webpack/src/title.js
+++ react-webpack/src/title.js
@@ -3,9 +3,14 @@
 import React from 'react'
 
 const Title = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'Desconhecido',
+    }
+  },
   render: function () {
-    return <h1>Olá {this.props.name}</h1>
-  }
+    return <h1>Olá {this.props.name}!</h1>
+  },
 })
 
 export default Title
