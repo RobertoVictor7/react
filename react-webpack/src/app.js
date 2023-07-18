813511d666c5624f55dff91cefd94e54739895df
Victor Roberto <robertovictor0702@gmail.com>
2023-07-17 23:12:33 -0300

    passando props

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,10 +1,15 @@
 'use strict'
 
 import React from 'react'
+import Title from './title'
 
 const App = React.createClass({
   render: function () {
-    return <h1>Aplicação</h1>
+    return (
+      <h1>
+        <Title name='Victor Roberto' />
+      </h1>
+    )
   }
 })
 
