0241fe472462528f998db047d94e0073e071f818
Victor Roberto <robertovictor0702@gmail.com>
2023-07-26 15:44:54 -0300

    lifeCycle montagem/desmontagem componentes

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,26 +1,35 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Square from './square'
-import Button from './button'
+import Timer from './timer'
 
 class App extends Component {
   constructor () {
+    console.log('constructor')
     super()
     this.state = {
-      color: 'green'
+      showTimer: true
     }
   }
 
+  componentWillMount () {
+    console.log('componentWillMount')
+  }
+
+  componentDidMount () {
+    console.log('componentDidMount')
+  }
+
   render () {
+    console.log('render')
     return (
       <div className='container'>
+        {this.state.showTimer && <Timer />}
 
-        <Square color={this.state.color} />
-
-        {['red', 'green', 'blue'].map((color) => (
-          <Button key={color} handleClick={() => this.setState({ color })}>{color}</Button>))}
-
+        <button onClick={() => {
+          this.setState({ showTimer: !this.state.showTimer }) }}>
+          Show / hide timer
+        </button>
       </div>
     )
   }
