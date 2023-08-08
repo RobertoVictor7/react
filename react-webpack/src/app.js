4264444eab411bd81a05012945974c1aae4fd6f7
Victor Roberto <robertovictor0702@gmail.com>
2023-08-07 22:32:00 -0300

    Introducao a formularios com react

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,14 +1,29 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Button from './button'
+
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      value: 'Valor inicial'
+    }
+  }
+
   render () {
     return (
       <div>
-        <Button handleClick={() => console.log('click')}>
-          Clique em mim
-        </Button>
+        <form>
+          <input
+            type='text'
+            value={this.state.value}
+            onChange={(e) => {
+              this.setState({
+                value: e.target.value
+              })
+            }}
+          />
+        </form>
       </div>
     )
   }
