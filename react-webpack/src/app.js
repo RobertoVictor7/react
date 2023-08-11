bcf92b05b9cfa9ac1caf7df9a6c3d2d7ec9a8e16
Victor Roberto <robertovictor0702@gmail.com>
2023-08-10 22:02:55 -0300

    marcacao app github

--- react-webpack/src/app.js
+++ react-webpack/src/app.js
@@ -1,40 +1,45 @@
 'use strict'
 
-import React, { Component } from 'react'
+import React from 'react'
 
-class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      checked: false,
-      showContent: false
-    }
-  }
+const App = () => (
+  <div className='app'>
+    <div className='search'>
+      <input type='search' placeholder='Digite o nome do usuário do GitHub' />
+    </div>
 
-  render () {
-    return (
-      <div>
-        <label>
-          <input
-            type='checkbox'
-            checked={this.state.checked}
-            onChange={(e) => {
-              this.setState({
-                checked: !this.state.checked
-              }, () => {
-                this.setState({
-                  showContent: this.state.checked
-                })
-              })
-            }}
-          />{' '}
-          Mostrar Conteudo
-        </label>
+    <div className='user-info'>
+      <img src='https://avatars.githubusercontent.com/u/78868541?v=4' />
+    </div>
+    <h1>
+      <a href='https://github.com/RobertoVictor7'>Victor Roberto de Andrade Oliveira</a>
+    </h1>
 
-        {this.state.showContent && <div>Olha eu aqui!</div>}
-      </div>
-    )
-  }
-}
+    <ul className='repos-info'>
+      <li> Repositórios: 122 </li>
+      <li> Seguidores: 10 </li>
+      <li> Seguindo: 11 </li>
+    </ul>
+
+    <div className='actions'>
+      <button>Ver repositórios</button>
+      <button>Ver favoritos</button>
+    </div>
+
+    <div className='repos'>
+      <h2>Repositórios:</h2>
+      <ul>
+        <li><a href='#'>Nome do repositório</a></li>
+      </ul>
+    </div>
+
+    <div className='starred'>
+      <h2>Favoritos:</h2>
+      <ul>
+        <li><a href='#'>Nome do repositório</a></li>
+      </ul>
+    </div>
+  </div>
+)
 
 export default App
